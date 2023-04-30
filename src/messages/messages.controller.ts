bbb646a3ba023716d375cd778043faff882b8355
Ghulam-Mustafa <themustafadeveloper@gmail.com>
2023-04-30 15:57:19 +0500

    till video 19

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller , Get , Param, Post} from '@nestjs/common';
+import { createMessagesDto } from './dtos/create-messages.dto';
+
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+    @Get()
+    listMessages(){
+        return 'HELLO MESSAGE'
+    }
+
+    @Post()
+    createMessages(@Body() body:createMessagesDto){
+        console.log(body)
+    }
+
+    @Get('/:id')
+    getMessages( @Param('id') id:string){
+        console.log(id)
+    }
+}
